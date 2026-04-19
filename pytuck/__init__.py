7729378b8465d256ed43091268a5226d402773e1
天霄 <go9sky@outlook.com>
2026-04-19 17:23:02 +0800

    chore(version): 更新项目版本号至1.3.0

--- pytuck/__init__.py
+++ pytuck/__init__.py
@@ -50,7 +50,7 @@ from .common.exceptions import (
 )
 from .common.options import SyncOptions, SyncResult
 
-__version__ = '1.2.1'
+__version__ = '1.3.0'
 __all__ = [
     # ==================== 推荐 API ====================
 
