f5dfa23c51dd263c0b54be1aa25bfd121ad4a60c
天霄 <go9sky@outlook.com>
2026-04-24 23:50:35 +0800

    chore(version): 正式发布v1.4.0

--- pytuck/__init__.py
+++ pytuck/__init__.py
@@ -51,7 +51,6 @@ from .common.exceptions import (
 from .common.options import SyncOptions, SyncResult
 
 __version__ = '1.4.0'
-
 __all__ = [
     # ==================== 推荐 API ====================
 
